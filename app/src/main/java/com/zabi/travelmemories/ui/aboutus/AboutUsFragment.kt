30d20ab55807959afceb8e67a6e57435b577755c
Zabi <zbvmihai@gmail.com>
2023-05-22 23:06:40 +0100

    About Us Fragment Finished

--- app/src/main/java/com/zabi/travelmemories/ui/aboutus/AboutUsFragment.kt
+++ app/src/main/java/com/zabi/travelmemories/ui/aboutus/AboutUsFragment.kt
@@ -13,8 +13,7 @@ class AboutUsFragment : Fragment() {
 
     private var _binding: FragmentAboutusBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,10 +27,17 @@ class AboutUsFragment : Fragment() {
         _binding = FragmentAboutusBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        aboutUsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val tvTitle: TextView = binding.tvTitle
+        aboutUsViewModel.title.observe(viewLifecycleOwner) {
+            tvTitle.text = it
         }
+
+        val tvAboutUs: TextView = binding.tvAbout
+        aboutUsViewModel.aboutus.observe(viewLifecycleOwner) {
+            tvAboutUs.text = it
+        }
+
+
         return root
     }
 
--- app/src/main/java/com/zabi/travelmemories/ui/aboutus/AboutUsViewModel.kt
+++ app/src/main/java/com/zabi/travelmemories/ui/aboutus/AboutUsViewModel.kt
@@ -6,8 +6,13 @@ import androidx.lifecycle.ViewModel
 
 class AboutUsViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is About Us Fragment"
+    private val _title = MutableLiveData<String>().apply {
+        value = "Travel Memories"
     }
-    val text: LiveData<String> = _text
+    val title: LiveData<String> = _title
+
+    private val _aboutus = MutableLiveData<String>().apply {
+        value = "Welcome to Travel Memories, the app that helps you save and share your best travel moments! Pin memories to maps, add notes, weather, and cool photos. Capture the essence of each place, preserve details with notes, embrace the weather, and bring memories to life with photos. Share your adventures with friends and family. Download Travel Memories now and create a collection of unforgettable travel moments. Let the journey begin!"
+    }
+    val aboutus: LiveData<String> = _aboutus
 }
